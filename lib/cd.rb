892ef7c613c283671be1242156394bdbda48ddcc
Cody Brubaker and Tracie Weitzman <tracie@>
2016-11-30 14:44:09 -0800

    passes six specs for CD title

--- lib/cd.rb
+++ lib/cd.rb
@@ -1,10 +1,40 @@
 class Organizer
+
+  @@cd_array = []
+
   define_method(:initialize) do |title|
     @title = title
+    @id = @@cd_array.length().+(1)
   end
 
   define_method(:title) do
     @title
+  end
+
+  define_singleton_method(:all) do
+    @@cd_array
+  end
+
+  define_method(:save) do
+    @@cd_array.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@cd_array = []
+  end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_singleton_method(:find) do |id|
+    found_cd = nil
+    @@cd_array.each() do |cd|
+      if cd.id().eql?(id)
+        found_cd = cd
+      end
+    end
+    found_cd
   end
 
 end
--- spec/cd_spec.rb
+++ spec/cd_spec.rb
@@ -2,11 +2,54 @@ require('rspec')
 require('cd')
 
 describe(Organizer) do
+  before() do
+    Organizer.clear()
+  end
 
   describe('#title') do
     it('returns the title of the CD') do
       test_organizer = Organizer.new("Greatest Hits")
       expect(test_organizer.title()).to(eq("Greatest Hits"))
+    end
+  end
+
+  describe('.all') do
+    it('array is empty at first') do
+      expect(Organizer.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves CD to array') do
+      test_organizer = Organizer.new("Greatest Hits")
+      test_organizer.save()
+      expect(Organizer.all()).to(eq([test_organizer]))
+    end
+  end
+
+  describe('.clear') do
+    it("empties out all of the saved CDs") do
+      Organizer.new("Greatest Hits").save()
+      Organizer.clear()
+      expect(Organizer.all()).to(eq([]))
+    end
+  end
+
+  describe('#id') do
+    it("returns the id of the title") do
+      test_organizer = Organizer.new("Greatest Hits")
+      test_organizer.save()
+      expect(test_organizer.id()).to(eq(1))
+    end
+  end
+
+  describe('.find') do
+    it("returns a CD by its ID number") do
+      test_organizer = Organizer.new("Greatest Hits")
+      test_organizer.save()
+      test_organizer2 = Organizer.new("Greatest Hits 2")
+      test_organizer2.save()
+      expect(Organizer.find(test_organizer.id())).to(eq(test_organizer))
     end
   end
 
